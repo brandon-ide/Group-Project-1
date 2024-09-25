6ad6995d496bfc93344813f4d9a90b190ba93232
Micah Howard <micahhoward99@gmail.com>
2024-09-24 21:34:59 -0400

    Sending my class

--- slider.js
+++ slider.js
@@ -91,6 +91,48 @@ function menu() {
   }
 }
 
+
+class Car {
+    constructor(make, model, year, description, price, picture) {
+        this.make = make;
+        this.model = model;
+        this.year = year;
+        this.description = description;
+        this.price = price;
+        this.picture = picture;
+    }
+}
+
+let carList = [
+    new Car("Cadillac", "CTS", 2013, "Luxurious and comfortable", 11264, "Assets/Cars/Cadillac.jpg"),
+    new Car("Toyota", "Supra", 1998, "Sporty and fast", 38222, "Assets/Cars/Toyota.jpg"),
+    new Car("Chevrolet", "Sonic", 2015, "Compact and affordable", 5115, "Assets/Cars/Chevrolet.jpg"),
+    new Car("BMW", "480i", 2020, "High performance", 52345, "Assets/Cars/BMW.jpg"),
+    new Car("Buick", "Encore", 2018, "Stylish and modern", 12980, "Assets/Cars/Buick.jpg"),
+    new Car("Mazda", "Miata", 1996, "Fun and sporty", 10496, "Assets/Cars/Mazda.jpg"),
+    new Car("Chevrolet", "Blazer", 2020, "Spacious SUV", 29995, "Assets/Cars/Blazer.jpg"),
+    new Car("Porsche", "911 GT3", 2024, "Luxury sports car", 182000, "Assets/Cars/Porsche.jpg"),
+];
+
+let carContainer = document.getElementById("car-container");
+
+carList.forEach(car => {
+    let carDiv = document.createElement("div");
+    carDiv.classList.add("slide", "card");
+
+    carDiv.innerHTML = `
+        <img src="${car.picture}" alt="${car.make}" width="330">
+        <h2>${car.make} ${car.model} (${car.year})</h2>
+        <p class="price">$${car.price.toLocaleString()}</p>
+        <p>${car.description}</p>
+        <p><button>Add to Cart</button></p>
+    `;
+    
+    carContainer.appendChild(carDiv);
+});
+
+
+
 //Shopping cart funcitonality
 // //First we check if we're on the page that needs to add content to the cart.
 // const addToCartButton = document.getElementById('addToCart');
@@ -111,4 +153,3 @@ function menu() {
 //     const storedPrice = localStorage.getItem('totalPrice');
 
 //     totalPriceElement.textContent = storedPrice ? `$${storedPrice}` : 'No items in cart';
-// }
